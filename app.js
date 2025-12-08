ff99598cd52b2df113f282b07e516fe75463cf58
sukmin1234 <128009214+sukmin1234@users.noreply.github.com>
2025-12-09 03:22:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -264,7 +264,8 @@ app.get('/', (req, res) => {
       <div class="container">
         <div class="header">
           <h1>🚀 ${APP_NAME}</h1>
-          <p>웹 애플리케이션 - TodoList, 연락처 폼</p>
+          
+          <p>최석민 | 20231541</p>
         </div>
 
         <div class="tabs">
